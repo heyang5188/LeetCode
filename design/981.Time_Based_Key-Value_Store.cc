c1bab4ab09af1c87d4a0199ead17e9cc4a573eae
Herb Scott <heyang5188@icloud.com>
2019-01-27 17:57:50 +0800

    121week 981

--- design/981.Time_Based_Key-Value_Store.cc
+++ design/981.Time_Based_Key-Value_Store.cc
@@ -1,8 +1,6 @@
-
-//https://leetcode-cn.com/contest/weekly-contest-121/problems/time-based-key-value-store/
+#include <map>
 #include <string>
 #include <unordered_map>
-#include <map>
 using namespace std;
 class TimeMap {
 public:
@@ -22,24 +20,19 @@ public:
     }
     void set(string key, string value, int timestamp)
     {
-        if (data.count(key) > 0) {
-            for (auto beg = data.lower_bound(key), end = data.upper_bound(key); beg != end; beg++) {
-                if (beg->second._str == value) {
-                    if (timestamp > beg->second._timestamp) {
-                        beg->second._timestamp = timestamp;
-                    }
-                    return;
+        for (auto beg = data.lower_bound(key), end = data.upper_bound(key); beg != end; beg++) {
+            if (beg->second._str == value) {
+                if (timestamp > beg->second._timestamp) {
+                    beg->second._timestamp = timestamp;
                 }
+                return;
             }
-            data.insert({ key, TimeNode(value, timestamp) });
-        } else {
-            data.insert({ key, TimeNode(value, timestamp) });
         }
+        data.insert({ key, TimeNode(value, timestamp) });
     }
     string get(string key, int timestamp)
     {
         TimeNode temp("", -2147483648);
-
         for (auto beg = data.lower_bound(key), end = data.upper_bound(key); beg != end; beg++) {
             if (beg->second._timestamp > temp._timestamp && beg->second._timestamp <= timestamp) {
                 temp = beg->second;
@@ -49,7 +42,6 @@ public:
     }
     std::multimap<string, TimeNode> data;
 };
-
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap* obj = new TimeMap();
